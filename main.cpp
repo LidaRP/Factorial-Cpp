141b5ef95155dd21bbc9c5a3279f633fa4fd4b70
LidaRP <rahmanipiyani.lida@gmail.com>
2022-02-25 16:06:47 +0330

    factorial

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	         int x,n=1;
+	         cout<<"Please enter a number:\n";
+	         cin>>x;
+	           for(int i=1;i<=x;i++)
+	              {
+	              	n*=i;
+				  }
+				    cout<<"Factorial is :"<<n;
+	
+return 0;
+           } 
